a9be59a6fcb86039ab3111798ef6b601c6ae54d4
Tom Koch <tzoxster@gmail.com>
2021-04-16 18:10:51 +0200

    enabled Admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-#from django.contrib.auth.models import PermissionMixin
+from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
@@ -26,14 +26,14 @@ class UserProfileManager(BaseUserManager):
 
         user = self.create_user(email, name, password)
 
-        user.is_superuser = AbstractBaseUser
-        user.is_staff = AbstractBaseUser
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using = self._db)
 
         return user
 
 
-class UserProfile(AbstractBaseUser):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """"Database model for users in the system"""
 
     email = models.EmailField(max_length = 255, unique = True)
@@ -46,13 +46,13 @@ class UserProfile(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
-    def get_fulle_name(self):
+    def get_full_name(self):
         """Retrieve full name of user"""
-        return self.NAME
+        return self.name
 
     def get_short_name(self):
         """Retrieve short name of user"""
-        return self.NAME
+        return self.name
 
     def __str__(self):
         """return string representation of user"""
