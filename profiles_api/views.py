2ed3c6d5f05a7d43268dbd4b577c0311bc5f3cd8
Tom Koch <tzoxster@gmail.com>
2021-04-16 22:03:36 +0200

    connected hello-world view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloAPIView(APIView):
+    """Test API View"""
+    def get(self, request, format = None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+        'Uses HTTP methos such as get,post...',
+        'Is similar to tradinationl Django view',
+        'Gives you the most control',
+        'Is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello!',
+                        'an_apiview': an_apiview})
